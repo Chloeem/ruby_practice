1ffafc4a5b89111648a9479ebcddf84d6a22eeb7
Chloë Emily Stokes <50859876+Chloeem@users.noreply.github.com>
2021-04-09 12:33:52 +0100

    Create vowel.count.rb
    Return the number (count) of vowels in the given string.
    
    We will consider a, e, i, o, u as vowels for this Kata (but not y).
    
    The input string will only consist of lower case letters and/or spaces.

---
+++ vowel.count.rb
@@ -0,0 +1,8 @@
+#Count up all the vowels in a given string
+#////////
+# 1. Count the the occurances of 'aeiou' - .count()
+# 2. Return this amount implicity 
+
+def get_count(input_str)
+  input_str.count('aeiou')
+end
