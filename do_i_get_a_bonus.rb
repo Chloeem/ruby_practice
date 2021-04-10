f8eb769a4a74c071ed4b2eb0f13eac09c196a6bf
Chloë Emily Stokes <50859876+Chloeem@users.noreply.github.com>
2021-04-10 16:37:06 +0100

    Update do_i_get_a_bonus.rb

--- do_i_get_a_bonus.rb
+++ do_i_get_a_bonus.rb
@@ -1,3 +1,11 @@
+#Workout what your salary is if you do or dont get a bonus
+#//////////
+# 1. Store witin a variable - total
+# 2. Using a ternary opertator check if the bonus is true
+# 3. If true, return salary * 10
+# 4. If false, return salary
+# 5. Concatonate to a dollar sign ($) to show total earnings
+
 def bonus_time(salary, bonus)
   total = bonus == true ? salary * 10 : salary
   "$#{total}"
