d82081eab03c4337ef63137572daab7fedad3a3a
Chloë Emily Stokes <50859876+Chloeem@users.noreply.github.com>
2021-04-10 16:43:32 +0100

    Create grasshopper_combine_strings.rb
    Create a function named (combine_names) that accepts two parameters (first and last name). The function should return the full name.

---
+++ grasshopper_combine_strings.rb
@@ -0,0 +1,7 @@
+#Build the a function which combines a first and last name with a space in between
+# 1. Build a function with multiple arguments available - *arg
+# 2. Join these together with a space in between - .join(' ')
+
+def combine_names (*names)
+  names.join(' ')
+end
