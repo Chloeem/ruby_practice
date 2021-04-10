24566aa71298851d89aae1ef734b7388e6265d7d
Chloë Emily Stokes <50859876+Chloeem@users.noreply.github.com>
2021-04-10 16:53:19 +0100

    Create how_many_lightsabers_do_you_own.rb
    Inspired by the development team at Vooza, write the function howManyLightsabersDoYouOwn/how_many_light_sabers_do_you_own that
    
    accepts the name of a programmer, and
    returns the number of lightsabers owned by that person.
    The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
    
    Note: your function should have a default parameter.

---
+++ how_many_lightsabers_do_you_own.rb
@@ -0,0 +1,10 @@
+#How many lightsabers do you own?
+#//////////
+# 1. Set a default parametre - name = ''
+# 2. Use a ternary opertator to check whether it is Zack
+# 3. If true, return 18
+# 3. If false, return 0
+
+def how_many_light_sabers_do_you_own(name = '')
+  name == 'Zach' ? 18 : 0   
+end
